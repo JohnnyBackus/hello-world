713cc65dbde7efc6982ab77b9c0c8df78f3bfb2f
JohnnyBackus <john.p.backus@gmail.com>
2023-10-12 13:33:09 -0700

    converted JS commands into functions and moved them to .js file

--- app.js
+++ app.js
@@ -1,54 +1,45 @@
-// some JS lecture notes as I follow along
+// function welcome(){
+//     let userAnswer = prompt('Hello! Welcome to my Sweet Potato Fan Page! Answer "yes" if you\'re ready to learn more');
+//     console.log ('First Response was: ' + userAnswer);
+//         if (userAnswer.toLowerCase() === 'yes' || userAnswer.toLowerCase() ==='"yes"') {
+//             document.write('Welcome fellow Sweetie!')
+//         // } else if (userAnswer.toLowerCase() === '"yes"') {
+//         //     document.write('Welcome fellow Sweetie!')
+//         } else (userAnswer = prompt('Oops! You misspelled "yes". Let\'s try again') {
+//             console.log ('Second Response was: ' + userAnswer);
+//             if (userAnswer.toLowerCase() === 'yes' || userAnswer.toLowerCase() ==='"yes"');
+//             document.write('Welcome fellow Sweetie!')
+//             }
+//             // } if (userAnswer.toLowerCase() == 'yes') {
+//             //     document.write('Welcome fellow Sweetie!')
+//             // } else if (userAnswer.toLowerCase() == '"yes"') {
+//             //     document.write('Welcome fellow Sweetie!')
+//             } else {                       
+//                 document.write('Oops! You misspelled "yes" again or maybe you meant to visit the Team Russet fanpage linked below. Either way, spend a moment here, and you will become a fanatic of the amazing sweet potato!')
+//             }
+//         }}
 
-// A variable is a container for information (data)
+function welcome() {
+    let userAnswer = prompt('Hello! Welcome to my Sweet Potato Fan Page! Answer "yes" if you\'re ready to learn more');
+    console.log('First Response was: ' + userAnswer);
 
-// 4 ways to declare a variable...
+    if (userAnswer.toLowerCase() === 'yes' || userAnswer.toLowerCase() === '"yes"') {
+        document.write('Welcome fellow Sweetie!');
+    } else {
+        userAnswer = prompt('Oops! You misspelled "yes". Let\'s try again');
+        console.log('Second Response was: ' + userAnswer);
 
-// 1. let
-// 2. cont
-// 3. var <-- Don't use this
-// 4. (none) <-- Don't use this
-
-// const --> a constant, unchangeable variable
-// let --> can be changed later on
-
-// DATA TYPE
-// strings: text - has quitation marks
-    // 'Kassie' -- '32' -- 'Hello world'
-
-// boolean: True Or False
-
-// number: no quotation marks
-    // 32
-
-// = is an assignment operator; it is not the same as a math function
-    // == is the math function and can be used to evaluate if a statement is True
-        // let x = 5
-        // let y = 10
-        // x == y returns False
-
-        // let x = 5
-        // let y = '5'
-        // x==y returns True (LOOSELY equivalent)
-        //  x===y returns False (STRICTLY equivalent; DATA TYPES must match)
-
-// SOME COMMANDS TO LEARN
-
-// alert('INSERT TEXT HERE'); -- This creates a pop up dialog box
-// const userAnswer = prompt('INSERT PROMPT/QUESTION HERE'); -- Creates a query dialog box and stores the response as a variable called 'userAnswer'
-// console.log('user typed: ' + userAnswer); -- Allows you to easily view user response to prompt in webpage console
-    // 'user typed: ' is optional, but allows you to easily identify the use input in the browser console
-// document.write('INSERT TEXT HERE'); -- Adds text to webpage wherever you insert a script tag into the html file
+        if (userAnswer.toLowerCase() === 'yes' || userAnswer.toLowerCase() === '"yes"') {
+            document.write('Welcome fellow Sweetie!');
+        } else {
+            document.write('Uh oh! You misspelled "yes" again or maybe you meant to visit the Team Russet fanpage linked below. Either way, spend a moment here, and you will become a fanatic of the amazing sweet potato!');
+        }
+    }
+}
 
 
-// CONDITIONAL LOGIC
-// If () {} -- space in between for readability
-    // If (this is True) then execute {command}
-    //  if (x==y) {
-    //     console.log('Yes it is')
-    //  } else {
-    //     console.log('No it is not')
-    //  }
-
-// else if () {} allows for more response commands that are specific to an input (or range of inputs)
-// else () {} allows for repsonses to all other input 
+function taterAge(){
+    let userAnswer = prompt('Did you know that the average sweet potato plant is 300 years old!? Want to find out your age in sweet potato years? Enter your age (numerals only, please) and click the button below!');
+    console.log ('Age Response was: ' + userAnswer);
+    return parseInt(userAnswer) / 4;
+}
